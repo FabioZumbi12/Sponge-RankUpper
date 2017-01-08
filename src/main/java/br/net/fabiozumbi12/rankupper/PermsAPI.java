d18b438893d2c2ce0efac04a4fdd16e2c8c98161
codeHusky <lokio27productions@gmail.com>
2017-01-08 15:26:06 -0600

    Fixed up something.

--- src/main/java/br/net/fabiozumbi12/rankupper/PermsAPI.java
+++ src/main/java/br/net/fabiozumbi12/rankupper/PermsAPI.java
@@ -1,13 +1,13 @@
 package br.net.fabiozumbi12.rankupper;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PermsAPI {
 	private PermissionService permissionService;
@@ -18,12 +18,28 @@ public class PermsAPI {
 	
 	public List<String> getGroups(User player){
 		List<String> gps = new ArrayList<String>();
-		for (Subject sub:player.getParents()){			
+		for (Subject sub:player.getParents()){
 			if (sub.getContainingCollection().equals(getGroups()) && (sub.getIdentifier() != null)){
 				gps.add(sub.getIdentifier());
 			}
 		}
-		return gps;	
+		return gps;
+	}
+	public String getHighestGroup(User player){
+		String best = "";
+		int beb = -1;
+		ArrayList<String> prevPerms = new ArrayList<>();
+		for (Subject sub:player.getParents()){
+			if (sub.getContainingCollection().equals(getGroups()) && (sub.getIdentifier() != null)){
+				System.out.println(sub.getIdentifier() + " - " + sub.getParents().size());
+				if(beb < sub.getParents().size()){
+					beb = sub.getParents().size();
+					best = sub.getIdentifier();
+				}
+			}
+		}
+		//return gps;
+		return best;
 	}
 	
 	public SubjectCollection getGroups(){
